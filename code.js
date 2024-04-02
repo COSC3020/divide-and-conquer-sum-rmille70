9e899b3753da3c7b6991ff184036f2260019c2ba
Ryan Miller <rmille70@uwyo.edu>
2024-04-02 11:43:40 -0600

    Handled Three Cases in One

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-
 function divideAndConquerSum(a){
     if (a.length <= 3){
         let sum = 0;
@@ -6,17 +5,17 @@ function divideAndConquerSum(a){
             sum += a[i];
         }
         return sum;
-    } if (a.length % 3 == 0){
-        return divideAndConquerSum(a.slice(0,(a.length/3))) + 
-        divideAndConquerSum(a.slice((a.length/3), (2*(a.length/3)))) + 
-        divideAndConquerSum(a.slice((2*(a.length/3)),(3*(a.length/3))));
-    } else if (a.length % 3 == 1) {
-        return divideAndConquerSum(a.slice(0,((a.length-1)/3))) + 
-        divideAndConquerSum(a.slice(((a.length-1)/3), (2*(a.length-1)/3))) + 
-        divideAndConquerSum(a.slice((2*(a.length-1)/3),((3*(a.length-1)/3)+1)));
     } else {
-        return divideAndConquerSum(a.slice(0,((a.length-2)/3))) + 
-        divideAndConquerSum(a.slice(((a.length-2)/3), (2*((a.length-2)/3)+1))) + 
-        divideAndConquerSum(a.slice((2*(a.length-2)/3)+1),((3*(a.length-2)/3)+2));
+        let ind1 = Math.floor(a.length/3);
+        let ind2 = (2*(ind1));
+        let ind3 = (3*(ind1));
+        while(ind3 != a.length){
+            ind1++;
+            ind2++;
+            ind3++;
+        }
+        return divideAndConquerSum(a.slice(0,ind1)) + 
+        divideAndConquerSum(a.slice(ind1,ind2)) + 
+        divideAndConquerSum(a.slice(ind2,ind3));
     }
 }
